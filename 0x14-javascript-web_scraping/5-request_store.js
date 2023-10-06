87a20ff2dd297923710888bcdf7323c4132af54d
Isaac Olumide Ogunfolaju <zicogunfolaju@gmail.com>
2023-10-06 15:06:07 +0000

    Add request store script

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(process.argv[3], body, 'utf8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
